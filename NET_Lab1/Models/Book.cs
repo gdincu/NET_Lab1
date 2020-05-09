81ad93853a839e414b763748b17a64760e43b6df
gdincu <dici0137@scs.ubbcluj.ro>
2020-05-09 15:52:24 +0300

    Add project files.

---
+++ NET_Lab1/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NET_Lab1.Models
+{
+    public class Book
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int YearOfPublication { get; set; }
+        public double Price{ get; set; }
+    }
+}
---
+++ NET_Lab1/Startup.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+//using Microsoft.EntityFrameworkCore;
+using FileContextCore;
+using NET_Lab1.Models;
+
+namespace NET_Lab1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt =>  // TODO ask about syntax here
+            //Folosit pentru a salva datele local 
+            opt.UseFileContextDatabase("Books"));
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
